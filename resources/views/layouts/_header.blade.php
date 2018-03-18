f3208e685a560b86749c4e4e5d0f15179fcad9c6
gakkispy <yaosenjun168@live.cn>
2018-03-18 17:31:56 +0000

    User can upload avatar

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -33,7 +33,7 @@
                     <li class="dropdown">
                         <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="dropdown-toggle">
                             <span class="user-avatar pull-left" style="margin-right:8px; margin-top:-5px;">
-                                <img src="https://fsdhubcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" alt="avatar" class="img-response img-circle" width="30px" height="30px">
+                                <img src="{{ $user->avatar }}" alt="avatar" class="img-response img-circle" width="30px" height="30px">
                             </span>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
