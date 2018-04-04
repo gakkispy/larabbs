def8c00f431e978d7c4a92c7e2944406ae9b2919
gakkispy <yaosenjun168@live.cn>
2018-04-04 13:27:35 +0000

    List user replies

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -42,7 +42,7 @@
                     <li class="{{ active_class(if_query('tab', null)) }}">
                         <a href="{{ route('users.show', $user->id) }}">Ta 的话题</a>
                     </li>
-                    <li class="{{ active_class(if_query('tab', replies)) }}">
+                    <li class="{{ active_class(if_query('tab', 'replies')) }}">
                         <a href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">Ta 的回复</a>
                     </li>
                 </ul>
